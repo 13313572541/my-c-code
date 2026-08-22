71b7029b258ff059618c51eb3f9ca031e7d1684d
林下人 <13313572541@qq.com>
2026-08-22 22:56:11 +0800

    move VS project into repo: add TestProject with question.c and usual.c

---
+++ TestProject/Project1/FileName.c
@@ -0,0 +1,28 @@
+#define  _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	// Your code here
+	int p = 0;
+	int b = 0;
+	//shu ru
+	printf("need");
+	scanf("%d", &p);
+	printf("u give:");
+	scanf("%d", &b);
+
+	if (b < p)
+	{
+		printf("need%d\n", p - b);
+	}
+	else if (b > p)
+	{
+		printf("u get%d元\n", b - p);
+	}
+	else
+	{
+		printf("OK\n");
+	}
+
+}

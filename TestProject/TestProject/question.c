d0282f9cacecb129c728468cdcab97ecf131a817
林下人 <13313572541@qq.com>
2026-08-24 10:33:43 +0800

    完成T-H: findMax+findMin 求最大最小差

--- TestProject/TestProject/question.c
+++ TestProject/TestProject/question.c
@@ -2,21 +2,33 @@
 #include <stdio.h>
 
 int findMax(int a[], int n) {
-	int max = a[0];
-	for (int i = 1; i < n; i++) {
-		if (a[i] > max) {
-			max = a[i];
-		}
-	}
-	return max;	
+    int max = a[0];
+    for (int i = 1; i < n; i++) {
+        if (a[i] > max) {
+            max = a[i];
+        }
+    }
+    return max;
+}
+
+int findMin(int a[], int n) {
+    int min = a[0];
+    for (int i = 1; i < n; i++) {
+        if (a[i] < min) {
+            min = a[i];
+        }
+    }
+    return min;
 }
 
 int main() {
-	int a[10];
-	scanf("%d %d %d %d %d %d %d %d %d %d", &a[0], &a[1], &a[2], &a[3], &a[4], &a[5], &a[6], &a[7], &a[8], &a[9]);
-	
-	int max = findMax(a, 10);
-	printf("The maximum element is: %d\n", max);
+    int a[10];
+    scanf("%d %d %d %d %d %d %d %d %d %d", &a[0], &a[1], &a[2], &a[3], &a[4], &a[5], &a[6], &a[7], &a[8], &a[9]);
+
+    int max = findMax(a, 10);
+    int min = findMin(a, 10);
+
+    printf("max-min: %d\n", max - min);
 
     return 0;
 }
